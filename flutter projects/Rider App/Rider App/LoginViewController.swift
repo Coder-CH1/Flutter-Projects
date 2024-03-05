5acc3b9630d7f028b297f098f6eb54ab88d27a4d
Coder-CH1 <agbachichidiogomelvis@gmail.com>
2024-03-05 19:38:22 +0100

    added

---
+++ flutter projects/Rider App/Rider App/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  Rider App
+//
+//  Created by Mac on 17/09/2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ flutter projects/Rider App/Rider App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rider App
+//
+//  Created by Mac on 16/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
